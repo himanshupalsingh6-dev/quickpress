4df0dca3e28cf2e186ff39a997530070f2e39f63
Himanshu pal <himanshupalsingh6@gmail.com>
2026-01-26 08:55:10 +0530

    Create notify.js

---
+++ assets/js/notify.js
@@ -0,0 +1,14 @@
+/*
+  SIMPLE NOTIFICATION SYSTEM
+  (Browser based – beginner safe)
+*/
+
+export function notifyUser(title, message) {
+  alert(`🔔 ${title}\n\n${message}`);
+}
+
+/*
+ Examples:
+ notifyUser("New Order", "You have a new ironing order");
+ notifyUser("Delivery Assigned", "Pickup order from hub");
+*/
