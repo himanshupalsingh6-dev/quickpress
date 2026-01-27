09162db6d8f8875eefb162f65ffe05b67797ec8d
Himanshu pal <himanshupalsingh6@gmail.com>
2026-01-27 19:35:24 +0530

    Update cart.js

--- assets/cart.js
+++ assets/cart.js
@@ -1,33 +1,44 @@
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-export function addToCart(product){
-  const item = cart.find(p=>p.id===product.id);
-  if(item){
+function save() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+export function addToCart(product) {
+  const item = cart.find(p => p.id === product.id);
+  if (item) {
     item.qty += 1;
-  }else{
-    cart.push({...product, qty:1});
+  } else {
+    cart.push({ ...product, qty: 1 });
   }
   save();
 }
 
-export function changeQty(id,delta){
-  const item = cart.find(p=>p.id===id);
-  if(!item) return;
-  item.qty += delta;
-  if(item.qty <= 0){
-    cart = cart.filter(p=>p.id!==id);
+export function increaseQty(id) {
+  const item = cart.find(p => p.id === id);
+  if (!item) return;
+  item.qty += 1;
+  save();
+}
+
+export function decreaseQty(id) {
+  const item = cart.find(p => p.id === id);
+  if (!item) return;
+  item.qty -= 1;
+  if (item.qty <= 0) {
+    cart = cart.filter(p => p.id !== id);
   }
   save();
 }
 
-export function getTotal(){
+export function getCart() {
+  return cart;
+}
+
+export function getTotal() {
   let total = 0;
-  cart.forEach(i=>{
+  cart.forEach(i => {
     total += i.price * i.qty;
   });
   return total;
-}
-
-function save(){
-  localStorage.setItem("cart",JSON.stringify(cart));
 }
